617e108b6b8e53fcc8795345b4db2dd9aa8c4fad
Henrique <henrique-200@outlook.com>
2025-09-03 17:14:04 -0300

    Subindo backend na AWS

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ app = FastAPI()
 class UserRequest(BaseModel):
     user: str
 
+
+@app.get("/auth/me")
+async def read_me():
+    return {"status": "Hello World"}
+
 @app.post("/auth/me")
 async def auth_me(req: UserRequest):
     return {"user": req.user, "ping": "pong"}
